de08afc87922518669b48f947514f9386ce7c593
Jarmil <jarmil.halamicek@seznam.cz>
2018-09-02 21:40:08 +0200

    Vytvarej statistiky poctu clenu po krajich a celkem

--- src/pistat.py
+++ src/pistat.py
@@ -12,6 +12,27 @@
 from func import *
 import credentials
 
+
+# Pro zjistovani poctu clenu: id statistiky a URL na forum prislusne skupiny KS
+PIRATI_KS = { 
+	"PI_MEMBERS_ZLINSKY": "https://forum.pirati.cz/memberlist.php?mode=group&g=39",
+	"PI_MEMBERS_VYSOCINA": "https://forum.pirati.cz/memberlist.php?mode=group&g=37",
+	"PI_MEMBERS_USTECKY": "https://forum.pirati.cz/memberlist.php?mode=group&g=42",
+	"PI_MEMBERS_STREDOCESKY": "https://forum.pirati.cz/memberlist.php?mode=group&g=78",
+	"PI_MEMBERS_PRAHA_HOST": "https://forum.pirati.cz/memberlist.php?mode=group&g=416",
+	"PI_MEMBERS_PRAHA": "https://forum.pirati.cz/memberlist.php?mode=group&g=33",
+	"PI_MEMBERS_PLZENSKY": "https://forum.pirati.cz/memberlist.php?mode=group&g=44",
+	"PI_MEMBERS_PARDUBICKY": "https://forum.pirati.cz/memberlist.php?mode=group&g=35",
+	"PI_MEMBERS_OLOMOUCKY": "https://forum.pirati.cz/memberlist.php?mode=group&g=38",
+	"PI_MEMBERS_MORAVSKOSLEZSKY": "https://forum.pirati.cz/memberlist.php?mode=group&g=34",
+	"PI_MEMBERS_LIBERECKY": "https://forum.pirati.cz/memberlist.php?mode=group&g=41",
+	"PI_MEMBERS_KRALOVEHRADECKY": "https://forum.pirati.cz/memberlist.php?mode=group&g=32",
+	"PI_MEMBERS_KARLOVARSKY": "https://forum.pirati.cz/memberlist.php?mode=group&g=43",
+	"PI_MEMBERS_JIHOMORAVSKY": "https://forum.pirati.cz/memberlist.php?mode=group&g=36",
+	"PI_MEMBERS_JIHOCESKY": "https://forum.pirati.cz/memberlist.php?mode=group&g=40",
+	"PI_MEMBERS_NEZARAZENI": "https://forum.pirati.cz/memberlist.php?mode=group&g=437"
+	}
+
 # Syntax one-liners	
 def arg(argumentName):	return getArg(argumentName,"tv")
 
@@ -32,6 +53,25 @@ def statFioBalance(account):
 		if balance>=0:
 			Stat(dbx, "BALANCE_%s" % account, balance, 0, "Stav uctu %s" % account)		
 		
+def statNrOfMembers(id, url):
+	""" Statistika poctu clenu krajsekho sdruzeni, zjistuje se z piratskeho fora,
+		jako pocet clenu dane skupiny, vypsany na samostatnem radku "XX uzivatelu"
+	"""
+	lines = grep(getLines(url), r'[0-9]{1,5} uživatel')
+	if len(lines)==1:
+		lineparts = lines[0].strip().split()
+		try:
+			count = int(lineparts[0])
+		except ValueError:
+			count = 0
+			
+		Stat(dbx, id, count, 0, 'Pocet clenu v %s' % id[11:])	
+	else:
+		print("Error: Nemohu najit pocet clenu %s: %s " % (id,url))
+		count = 0
+	
+	return count	
+		
 ##############################################################################################################
 # KOD
 ##############################################################################################################
@@ -49,14 +89,18 @@ def main():
 	# Zustatky na transparentnich FIO uctech
 	for account in ( "2100643125", "2100048174", "2901172853", "2400643143", "2100643205", "2400643151", "2700643161" ):
 		statFioBalance(account)
-			
 
+	# Pocty clenu v jednotlivych KS a celkem ve strane (prosty soucet dilcich)		
+	sum = 0
+	for id in PIRATI_KS:
+		sum += statNrOfMembers(id, PIRATI_KS[id])
+	Stat(dbx, "PI_MEMBERS_TOTAL", sum, 0, 'Pocet clenu CPS celkem')			
+
+			
 def test():
 	""" Zde se testuji nove statistiky, spousti se s parametrem -t """
-	for account in ( "2100643125", "2100048174", "2901172853", "2400643143", "2100643205", "2400643151", "2700643161" ):
-		PrintLastValues(dbx,"BALANCE_%s" % account,4)
 	pass
-			
+
 	
 if __name__ == '__main__': 
 	dbx = clsMySql(credentials.FREEDB, verbose=arg('v'))
