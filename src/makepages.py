46e5af2af4da0644294ba419db3c814461794eb6
Jarmil <jarmil.halamicek@seznam.cz>
2019-02-01 10:31:24 +0100

    Dvojite statistiky pro twitter a hezci graf

--- src/makepages.py
+++ src/makepages.py
@@ -13,6 +13,9 @@ import func
 import datetime
 import credentials
 import shutil
+import re
+
+LINE_COLORS = ('b', 'g', 'r', 'c', 'k')
 
 def arg(argumentName):
     return func.getArg(argumentName,"ho:b:")
@@ -25,31 +28,54 @@ def message_and_exit(message=""):
     exit()
 
 
-def make_graph( rows, stat_id, filename=""):
+def make_graph( rowlist, stat_id, filename=""):
     """ Vytvori carovy graf. Ulozi jej do souboru,
         neni-li jmeno souboru definovano, zobrazi jej
+        rowlist .. list s datovymi radami
     """    
 
     # HACK: proste hnusne, prepsat pythonic way
-    X, Y = [], []
-    for row in rows:
-        X.append('{0:%d.%m.%Y}'.format(row[0]))
-        Y.append(row[1])
+    datarows = {}
+    for rows in rowlist.keys():
+        X, Y = [], []
+        for row in rowlist[rows]:
+            X.append('{0:%d.%m.%Y}'.format(row[0]))
+            Y.append(row[1])
+        datarows[rows] = (X,Y)
 
     # create graph
     figure(num=None, figsize=(16, 10), dpi=80, facecolor='w', edgecolor='w')
     ax = plt.axes()
     ax.xaxis.set_major_locator(plt.MaxNLocator(6))  # pocet ticku na X ose
-    plt.plot(X, Y, 'k-', linewidth=3.0) 
-    plt.ylim(bottom=0)  # osa Y zacina vzdy od nuly
+    
+    i = 0
+    for key in datarows.keys():
+        plt.plot(datarows[key][0], datarows[key][1], '%s-' % LINE_COLORS[i], linewidth=4.0, label=key) 
+        i += 1
+        
+    ax.spines['top'].set_visible(False)             # odstran horni a pravy ramecek grafu
+    ax.spines['right'].set_visible(False)
+    plt.ylim(bottom=0)                              # osa Y zacina vzdy od nuly
     plt.ticklabel_format(style='plain', axis='y')
     plt.tick_params(axis='both', which='major', labelsize=16) # velikost fontu na osach
+
+    if len(rowlist) > 1: 
+        ax.legend()
+
     if filename:
         plt.savefig(filename, bbox_inches='tight')
     else:
         plt.show()
     
     plt.close()   # pri vetsim poctu grafu nutne (memory leak)  
+
+
+def get_stat_for_graph(dbx, stat):
+    """ Vrati seznam: vsechna data grafu, nejstarsi prvni """
+    stat_object = func.clsMyStat(dbx, stat)
+    r = stat_object.getLastValues(0)
+    r.reverse()
+    return r
 
 
 def make_pages(dbx, dirname):
@@ -66,37 +92,59 @@ def make_pages(dbx, dirname):
     statnames = func.readfile('../config/statnames').split('\n')
 
     i, index = 0, ['','']
+
+    # Statistiky twitteru: kombinuj do spolecneho grafu TWEETS & FOLLOWERS
+    for stat in stats: 
+        found = re.search(r'TWITTER_(.+?)_TWEETS', stat)
+        if found: 
+            statname = "TWITTER_%s" % found.group(1)
+            print("Creating %s                       \r" % (statname), end = '\r')
+            
+            # graf
+            data_tweets = get_stat_for_graph(dbx, stat)
+            data_followers = get_stat_for_graph(dbx, "TWITTER_%s_FOLLOWERS" % found.group(1))
+            make_graph( { "TWEETS": data_tweets, "FOLLOWERS": data_followers }, stat, "%s/img/%s.png" % (dirname, statname) )
+            
+            # html stranka: bez zdrojovych dat
+            page = func.replace_all(func.readfile('../templates/stat_nodata.htm'),
+                { '%stat_name%': statname, '%stat_desc%': '', '%stat_image%': "img/%s.png" % statname,
+                '%stat_id%': statname, '%stat_date%': '{0:%d.%m.%Y %H:%M:%S}'.format(datetime.datetime.now()) } )
+            func.writefile(page, "%s/%s.htm" % (dirname, statname))    
+            
+            index[1] += "<a href='%s.htm'>%s</a>, \n" % (statname, statname)
+        
+    #exit()    
+
     for stat in stats: 
         i += 1
         print("[%s/%s]: Creating %s                       \r" % (i, len(stats), stat), end = '\r')
-
-        # ziskej statistiku
-        stat_object = func.clsMyStat(dbx, stat)
-        r = stat_object.getLastValues(0)
-        r.reverse()
-
-        make_graph(r, stat, "%s/img/%s.png" % (dirname, stat) )
         
-        # najdi pro ID statistiky odpovidajici radek ve statnames, 
-        # nacti z nej nazev, dulezitost...
-        info = list(filter(lambda x: x.startswith('%s\t' % stat ), statnames))
-        statname = info[0].split('\t')[1] if info else stat
-        if statname.startswith('!'):
-            important = True
-            statname = statname[1:]
-        else:
-            important = False
+        if not re.search(r'TWITTER_(.+)', stat):
 
-        # vytvor html soubor se zobrazenim statistiky
-        page = func.replace_all(func.readfile('../templates/stat.htm'),
-            { '%stat_name%': statname, '%stat_desc%': '', '%stat_image%': "img/%s.png" % stat,
-              '%stat_id%': stat, '%stat_date%': '{0:%d.%m.%Y %H:%M:%S}'.format(datetime.datetime.now()) } )
-        func.writefile(page, "%s/%s.htm" % (dirname, stat))    
-        index[0 if important else 1] += "<a href='%s.htm'>%s</a>, \n" % (stat, statname)
-        
-        # vytvor CSV soubor se zdrojovymi daty
-        csv_rows = [ "%s;%s;%s;" % (stat, "{:%d.%m.%Y}".format(x[0]), x[1]) for x in r ]
-        func.writefile("stat_id;date;value;\n" + "\n".join(csv_rows), "%s/%s.csv" % (dirname, stat))    
+            # ziskej statistiku a vytvor graf
+            r = get_stat_for_graph(dbx, stat)
+            make_graph({ "graph": r }, stat, "%s/img/%s.png" % (dirname, stat) )
+            
+            # najdi pro ID statistiky odpovidajici radek ve statnames, 
+            # nacti z nej nazev, dulezitost...
+            info = list(filter(lambda x: x.startswith('%s\t' % stat ), statnames))
+            statname = info[0].split('\t')[1] if info else stat
+            if statname.startswith('!'):
+                important = True
+                statname = statname[1:]
+            else:
+                important = False
+
+            # vytvor html soubor se zobrazenim statistiky
+            page = func.replace_all(func.readfile('../templates/stat.htm'),
+                { '%stat_name%': statname, '%stat_desc%': '', '%stat_image%': "img/%s.png" % stat,
+                  '%stat_id%': stat, '%stat_date%': '{0:%d.%m.%Y %H:%M:%S}'.format(datetime.datetime.now()) } )
+            func.writefile(page, "%s/%s.htm" % (dirname, stat))    
+            index[0 if important else 1] += "<a href='%s.htm'>%s</a>, \n" % (stat, statname)
+            
+            # vytvor CSV soubor se zdrojovymi daty
+            csv_rows = [ "%s;%s;%s;" % (stat, "{:%d.%m.%Y}".format(x[0]), x[1]) for x in r ]
+            func.writefile("stat_id;date;value;\n" + "\n".join(csv_rows), "%s/%s.csv" % (dirname, stat))    
 
 
     page = func.replace_all(func.readfile('../templates/index.htm'), { '%important%': index[0], '%body%': index[1] } )
