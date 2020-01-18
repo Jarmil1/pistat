20cbc4b66ea2f17f0fc367a7bd94aff9633675cc
Jarmil <jarmil.halamicek@seznam.cz>
2020-01-18 21:29:31 +0100

    pocatek importu:

--- src/tmp.py
+++ src/tmp.py
@@ -2,38 +2,22 @@
 
 """ 
     Some tests
-    
 """
 
 from func import *
 import func
 import re 
-import shutil
-import random
-import os
+import credentials
 
-def _dummy_child(name, element):
-    """ Vrat prvni XML element, jehoz tag konci na 'name'.
-        HACK kvuli dekorovanym jmenum tagu: obsahuji {atom}
-    """    
-    for child in element:
-        if child.tag.endswith(name):
-            return child
-    
 if __name__ == '__main__': 
 
-    # nacti vlakna z redmine, vypis nazvy prispevku a pocet
-    # TODO: Atom redmine vraci pouze 15 (asi) nejnovejsich prispevku, nelze tedy pres nej zjistovat pocty ukolu.
-    with open("../config/redmine","r") as f:
-        lines = f.readlines()    
+    dbx = func.clsMySql(credentials.LOCALHOST)
+    #dbx = func.PG(credentials.PGHOME, verbose=arg('v'))
 
-    for l in lines:
-        if not l.strip().startswith('#'):
-            m = re.search(r"[ \t]*(.*?)[ \t]+(.*)", l)
-            if m and len(m.groups())>1:
-                print(m.group(1),m.group(2))
-                entries  = func.atom_entries(m.group(2))
-                for entry in entries:
-                    cont = _dummy_child('title', entry).text.strip()
-                    print(cont)
-                print(len(entries))
+    r = dbx.fetchall("SELECT md5, description from METHODS")
+    for m,d in r:
+        print(m,d)
+
+
+    dbx.close()
+        
